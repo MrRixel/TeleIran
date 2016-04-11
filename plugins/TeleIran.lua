903f20dea4c0d9802d615813699a6470aac14f51
Armin <armin.bagheri.1380@gmail.com>
2016-04-11 19:59:11 +0430

    Update TeleIran.lua

--- plugins/TeleIran.lua
+++ plugins/TeleIran.lua
@@ -1,6 +1,6 @@
 do
 function run(msg, matches)
-  return " TeleIran V4 Based On Lua \n \n \n Developer: @TelePublic \n Channel: @TeleIranTeam \n\n WebSite: Www.TeleIran.Vcp.Ir"
+  return " TeleIran V5 Based On Lua \n \n \n Developer: @TelePublic \n Channel: @TeleIranTeam \n\n WebSite: Www.TeleIran.Vcp.Ir"
   end
 return {
   description = " TeleIran ", 
