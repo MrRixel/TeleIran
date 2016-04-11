fd6d00b8b2a760d4879700d7f347037ea59b3954
Armin <armin.bagheri.1380@gmail.com>
2016-04-11 20:30:01 +0430

    Update supergroup.lua

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -1258,7 +1258,7 @@ local function run(msg, matches)
 				resolve_username(username,  callbackres, cbres_extra)
 			else
 				savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested SuperGroup ID")
-				return "SuperGroup ID for " ..string.gsub(msg.to.print_name, "_", " ").. ":\n\n"..msg.to.id
+				return "#Chat_ID: "..msg.to.id.."\n#User_ID: "..msg.from.id
 			end
 		end
 
